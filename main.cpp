2c98805c639b69853a1be59e136b820998071ef4
Eric Freda <eric.freda@yahoo.com>
2016-09-19 20:52:49 -0700

    Uploaded stuff
    Have not yet tested many of the functions and main.cpp is still unwritten.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "life.h"
+
+using namespace std;
+
+int main()
+{
+    
+    
+    
+    
+    
+    LifeBoard board;
+    
+    board.printBoard();
+    
+    
+}
---
+++ userinput.h
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+char getCharInput()
+{
+    string userInput;
+    while (true)
+    {
+        cin >> userInput;
+        if (userInput.length() == 1)
+        {
+            return userInput[0];
+        }
+        else
+        {
+            cout << "Must enter a single character." << endl;
+        }
+    }
+}
+
+int getIntInput()
+{
+    string userInput;
+    cin >> userInput;
+    int integer = 0;
+    int digit;
+    for (int i=1; i<=userInput.length(); ++i)
+    {
+        if (userInput[userInput.length()-i]=='0')
+        {
+            digit = 0;
+        }
+        else if (userInput[userInput.length()-i]=='1')
+        {
+            digit = 1;
+        }
+        else if (userInput[userInput.length()-i]=='2')
+        {
+            digit = 2;
+        }
+        else if (userInput[userInput.length()-i]=='3')
+        {
+            digit = 3;
+        }
+        else if (userInput[userInput.length()-i]=='4')
+        {
+            digit = 4;
+        }
+        else if (userInput[userInput.length()-i]=='5')
+        {
+            digit = 5;
+        }
+        else if (userInput[userInput.length()-i]=='6')
+        {
+            digit = 6;
+        }
+        else if (userInput[userInput.length()-i]=='7')
+        {
+            digit = 7;
+        }
+        else if (userInput[userInput.length()-i]=='8')
+        {
+            digit = 8;
+        }
+        else if (userInput[userInput.length()-i]=='9')
+        {
+            digit = 9;
+        }
+        else
+        {
+            cout << "Error: Not an integer. return 1" << endl;
+            return 1;
+        }
+        integer = integer + digit*pow(10, (i-1));
+    }
+    return integer;
+}
+
+float getProbInput()
+{
+    float userInput;
+    while (true)
+    {
+        if (cin >> userInput)
+        {
+            if (userInput<=1 and userInput>=0)
+            {
+                return userInput;
+            }
+            else
+            {
+                cout << "Error: Must be between zero and one. Try again." << endl;
+            }
+        }
+        else
+        {
+            cout << "Error: Must be a number. Try again." << endl;
+        }
+    }
+}
